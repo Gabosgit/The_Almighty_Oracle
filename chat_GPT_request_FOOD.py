4b24943f1528aa989ffa1e3ef85751ad0e70b2a8
Gabo <no@email>
2025-01-29 14:39:11 +0100

    .env file added with API_KEY | chat_GPT_request.py and  chat_GPT_request_FOOD.py were updated

--- chat_GPT_request_FOOD.py
+++ chat_GPT_request_FOOD.py
@@ -1,7 +1,11 @@
 import google.generativeai as genai
+import os
+from dotenv import load_dotenv
 
+load_dotenv() #loads variables from the .env file into the environment
+API_KEY = os.getenv('API_KEY') # os.getenv() to access the environment variables loaded from the .env file
 
-genai.configure(api_key="AIzaSyB0Zp5T1AOV9FzJ9e1MCWaPXVWYzQsJIkY")
+genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 #response = model.generate_content("Explain how AI works")
 
